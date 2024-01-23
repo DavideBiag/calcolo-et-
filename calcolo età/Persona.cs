77aeafbacd0c2295f543f0f217de7c12611ab394
Z1 <Z1@CASA-2022>
2024-01-23 17:40:59 +0100

    Aggiungere i file di progetto.

---
+++ calcolo età/Persona.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calcolo_età
+{
+    internal class Persona
+    {
+        public DateTime dataNascita { get; set; }
+
+        public int CalcoloEtà()
+        {
+            int anni= DateTime.Now.Year-dataNascita.Year;
+           
+            if(DateTime.Now.Month<dataNascita.Month || DateTime.Now.Month == dataNascita.Month && DateTime.Now.Day < dataNascita.Day)
+            {
+                anni = anni - 1;
+                return anni;
+            }
+            else
+            {
+                return anni;
+            }
+
+        }
+    }
+}
---
+++ calcolo età/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calcolo_età
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona p = new Persona();
+            Console.WriteLine("Inserisci la data di nascita:  ");
+            p.dataNascita= DateTime.Parse(Console.ReadLine());
+
+            Console.WriteLine( "hai {0} anni", p.CalcoloEtà());
+            Console.ReadLine(); 
+        }
+    }
+}
